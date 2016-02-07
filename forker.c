0c09ae45e7c2fd8a6963c845f11093500f1fed5b
raventid <juliankul@gmail.com>
2016-02-08 00:17:31 +0300

    fifo and other stuff

---
+++ forker.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+    if(fork()) {
+        if(fork()) {
+            if(fork()) {
+                fork();
+            }
+        }
+    }
+    printf("Hello!\n");
+
+    return 0;
+}
---
+++ parser_s.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+ char str[200];
+
+ gets(str);
+ system(str);
+ return 0;
+}
